7d5add57ad57e09eb3b485fb3463533586ee025d
Thomas Ritz <thomas.ritz@justrelate.com>
2021-06-07 11:27:42 +0200

    improve targeting widget

--- src/Widgets/TargetingWidget/TargetingWidgetComponent.js
+++ src/Widgets/TargetingWidget/TargetingWidgetComponent.js
@@ -1,51 +1,25 @@
-import * as React from "react";
+import React from "react";
 import * as Scrivito from "scrivito";
-import axios from "axios";
+import useFetch from "use-http";
 import "./TargetingWidget.scss";
 
-class TargetingWidgetComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { activityScore: 0, gender: "" };
-  }
+Scrivito.provideComponent("TargetingWidget", ({ _widget }) => {
+  const profileId = "3997c37116004f90b21230276ff61ed4";
+  const { data = {} } = useFetch(
+    `https://eva.crm.infopark.net/api2/profiles/${profileId}/targeting`,
+    {
+      cacheLife: 60000,
+      cachePolicy: "cache-first",
+      responseType: "json",
+      persist: true,
+    },
+    []
+  );
 
-  componentDidMount() {
-    getProfileTargetingData().then((state) => {
-      if (this.isUnmounted) {
-        return;
-      }
-      this.setState(state);
-      console.log("state in widget:", state);
-    });
-  }
-
-  componentWillUnmount() {
-    this.isUnmounted = true;
-  }
-
-  render() {
-    return (
-      <div className="targeting">
-        <div className="score">Score: {this.state.activityScore}</div>
-        <div className="gender">Gender: {this.state.gender}</div>
-      </div>
-    );
-  }
-}
-
-Scrivito.provideComponent("TargetingWidget", TargetingWidgetComponent);
-
-let cachedRequest;
-
-function getProfileTargetingData() {
-  if (!cachedRequest) {
-    const profileId = "3997c37116004f90b21230276ff61ed4";
-    cachedRequest = axios(
-      `https://eva.crm.infopark.net/api2/profiles/${profileId}/targeting`
-    ).then((response) => {
-      const { activity_score: activityScore, gender } = response.data;
-      return { activityScore, gender };
-    });
-  }
-  return cachedRequest;
-}
+  return (
+    <div className="targeting">
+      <div className="score">Score: {data.activity_score}</div>
+      <div className="gender">Gender: {data.gender}</div>
+    </div>
+  );
+});
