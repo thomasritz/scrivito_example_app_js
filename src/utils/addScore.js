0ab2171cfcf6c1486325ecfd090a3bffee88c987
Thomas Ritz <thomas.ritz@justrelate.com>
2021-06-08 10:44:08 +0200

    ignore add score errors

--- src/utils/addScore.js
+++ src/utils/addScore.js
@@ -1,10 +1,18 @@
 function addScore(eventType) {
   const profileId = localStorage.getItem("profile_id");
-  fetch(`https://eva.crm.infopark.net/api2/profiles/${profileId}/score`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ event_type: eventType }),
-  });
+  if (!profileId) {
+    return;
+  }
+  try {
+    fetch(`https://eva.crm.infopark.net/api2/profiles/${profileId}/score`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ event_type: eventType }),
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
 }
 
 export default addScore;
